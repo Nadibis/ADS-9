56ebb54ec928c0cc3446365f316d99d524031cf3
Nadibis <nadia041007@gmail.com>
2026-06-09 19:10:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,10 @@
 // Copyright 2022 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "tree.h"
 #include <algorithm>
+#include <cmath>
+#include <iostream>
 #include <queue>
+#include <vector>
+#include  "tree.h"
 
 PMTree::PMTree(const std::vector<char>& chars) {
     root = new Node('\0');
